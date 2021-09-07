29e349c18534b034bb3a8025968b6f13612d9b99
koop9087 <yarik4mo>
2021-09-07 15:10:37 +0300

    //

---
+++ src/by/academy/homework8/task2/Main.java
@@ -0,0 +1,10 @@
+package by.academy.homework8.task2;
+
+import by.academy.homework8.task2.service.ReflectionService;
+
+public class Main {
+    public static void main(String[] args) {
+        ReflectionService reflectionService = new ReflectionService();
+        reflectionService.callAllReflectService();
+    }
+}
---
+++ src/by/academy/homework8/task2/entity/User.java
@@ -0,0 +1,72 @@
+package by.academy.homework8.task2.entity;
+
+public class User extends Person {
+    private String login;
+    private String password;
+    private String email;
+
+    public User(String login, String password, String email) {
+        super();
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+    public void printUserInfo() {
+        System.out.println(this.login + " " + this.password + " " + this.email);
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        User user = (User) o;
+        if (login != null ? !login.equals(user.login) : user.login != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+        return email != null ? email.equals(user.email) : user.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (login != null ? login.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("login='").append(login).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
